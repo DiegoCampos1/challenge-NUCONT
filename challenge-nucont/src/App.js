a15eb00957ce82c70c775317d66b8d6cde32e76e
Diego Campos <diegodiko05@gmail.com>
2020-09-25 09:14:29 -0300

    Integrando Google Analytics, e medindo os acessos e mudança de rota

--- challenge-nucont/src/App.js
+++ challenge-nucont/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 import MainPage from './Pages/MainPage';
 import ThanksPage from './Pages/ThanksPage';
 
+ReactGA.initialize('UA-000000-01');
+
+const browserHistory = createBrowserHistory();
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search);
+  console.log(action, location);
+});
+
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
       <Switch>
